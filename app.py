22e5adceac01582847408dd7e6cb7248710cc99b
rotsenmaeva-lgtm <rotsenmaeva@gmail.com>
2026-02-14 01:37:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import streamlit as st
 import pandas as pd
+import csv
+from io import TextIOWrapper
+
+st.set_page_config(page_title="Contrôle Anomalies", layout="wide")
+st.title("🔍 Contrôle des anomalies fournisseurs")
+
+uploaded_file = st.file_uploader("Importer Export_Balance (CSV)", type="csv")
 # Read the CSV file, handling potential errors
 try:
-    df = pd.read_csv(file_path, encoding="UTF-16", on_bad_lines='skip')  # Skip bad lines
+    df = pd.read_csv(uploaded_file, encoding="UTF-16", on_bad_lines='skip')  # Skip bad lines
     # Or you can use 'error' to raise an error on bad lines
     # df = pd.read_csv(file_path, on_bad_lines='error')
 except pd.errors.ParserError as e:
